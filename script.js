5635e677b332a954b6edd97af3e1aee30d1c4b1f
Leonardo Capocci <leocap2001@gmail.com>
2024-04-23 14:38:47 -0700

    User can search specific cities

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-async function getWeather() {
+const searchBoxInput = document.querySelector("#search-box-input");
+const searchButton = document.querySelector("#search-button");
+
+async function getWeather(location) {
+  console.log("GETWEATHER FUNCTION");
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=ba2e280e87ad4e868ee165446241604&q=london`,
+    `https://api.weatherapi.com/v1/current.json?key=ba2e280e87ad4e868ee165446241604&q=${location}`,
     { mode: "cors" },
   );
   const weatherData = await response.json();
   console.log(weatherData);
+  return weatherData;
 }
 
-getWeather();
+searchButton.addEventListener("click", () => {
+  getWeather(searchBoxInput.value);
+});
