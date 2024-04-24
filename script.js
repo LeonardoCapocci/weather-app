3ac7e403647ee4a35f7e545416d158dc22af06d7
Leonardo Capocci <leocap2001@gmail.com>
2024-04-24 13:13:27 -0700

    locations are searchable, weather info is displayed on screen, replaced search button for return keypress

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const searchBoxInput = document.querySelector("#search-box-input");
-const searchButton = document.querySelector("#search-button");
+
+const condition = document.querySelector(".current-weather-conditions");
+const selectedLocation = document.querySelector(".selected-location");
+const temp = document.querySelector(".current-temp");
+const feelsLike = document.querySelector(".current-feels-like");
+const wind = document.querySelector(".current-wind");
+const humidity = document.querySelector(".current-humidity");
 
 async function getWeather(location) {
-  // console.log("GETWEATHER FUNCTION");
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=ba2e280e87ad4e868ee165446241604&q=${location}`,
     { mode: "cors" },
@@ -12,8 +17,23 @@ async function getWeather(location) {
   return weatherData;
 }
 
-function displayWeather() {
-  getWeather(searchBoxInput.value);
+async function displayWeather() {
+  const data = await getWeather(searchBoxInput.value);
+  const weatherData = data.current;
+  const locationData = data.location;
+
+  condition.textContent = weatherData.condition.text;
+  selectedLocation.textContent = `${locationData.name}, ${locationData.country}`;
+  temp.textContent = `${weatherData.temp_f}°F`;
+  feelsLike.textContent = `Feels Like: ${weatherData.feelslike_f}°F`;
+  wind.textContent = `Wind: ${weatherData.wind_mph}mph`;
+  humidity.textContent = `Humidity: ${weatherData.humidity}%`;
 }
 
-searchButton.addEventListener("click", displayWeather);
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    displayWeather();
+  }
+});
+
+displayWeather();
